8144dc041194412cce5756ec9acdfb2aca9597ca
NightGrimalkin <mateusz.mybox@gmail.com>
2022-02-07 07:38:08 +0100

    delete and checkout

--- src/ListElement.js
+++ src/ListElement.js
@@ -5,7 +5,6 @@ function ListElement(props) {
   return (
     <div className="listElement" >
       <p>Nr: {props.index} &nbsp;</p>
-      <input type="checkbox" className="check"/>
       <p>Produkt: {props.text} &nbsp;</p>
       <p>Cena: {props.price}zł &nbsp;</p>
       <p>Ilość: {props.amount} &nbsp;</p>
@@ -14,6 +13,9 @@ function ListElement(props) {
       </button>
       <button onClick={props.changeAmount} name="down" indexed={props.number}>
         -
+      </button>
+      <button onClick={props.del} indexed={props.number}>
+        delete
       </button>
     </div>
   );
--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -8,24 +8,39 @@ function ShoppingList() {
     listPriceInput: "",
   });
   const [notes, setNotes] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  function deleteListElement(e) {
+    const index = e.target.getAttribute("indexed");
+    let tempNotes = notes;
+    tempNotes.splice(index, 1);
+    setNotes([...tempNotes]);
+  }
 
   function changeAmount(e) {
     const index = e.target.getAttribute("indexed");
     const name = e.target.name;
     let tempNotes = notes;
     const basePrice = tempNotes[index].listPriceInput / tempNotes[index].amount;
+    const sumFix=tempNotes[index].listPriceInput = basePrice * tempNotes[index].amount;
     if (name === "up") {
-      console.log("jest");
       tempNotes[index].amount += 1;
       tempNotes[index].listPriceInput = basePrice * tempNotes[index].amount;
+
+
+      setSum((prevState)=>parseFloat(prevState-sumFix+tempNotes[index].listPriceInput));
+      setNotes([...tempNotes]);
     } else if (name === "down" && tempNotes[index].amount != 1) {
-      console.log("nie");
       tempNotes[index].amount -= 1;
       tempNotes[index].listPriceInput = basePrice * tempNotes[index].amount;
+
+      
+      setSum((prevState)=>parseFloat(prevState-sumFix+tempNotes[index].listPriceInput));
+      setNotes([...tempNotes]);
     } else {
       alert("Nie mozesz miec 0 produktów");
     }
-    setNotes([...tempNotes]);
+    
   }
 
   const handleInputChange = (e) => {
@@ -44,20 +59,25 @@ function ShoppingList() {
       amount: 1,
     });
     setNotes(temporaryNotes);
+    setSum((prevState)=>parseFloat(+prevState+parseFloat(tempTemporaryNotes.listPriceInput)));
     setTempNotes({
       listElementInput: "",
       listPriceInput: "",
     });
   }
-  function sortList(){
-    let tempNotes =notes;
-    tempNotes.sort(function(a, b){
-      if(a.listElementInput < b.listElementInput) { return -1; }
-      if(a.listElementInput > b.listElementInput) { return 1; }
+  function sortList() {
+    let tempNotes = notes;
+    tempNotes.sort(function (a, b) {
+      if (a.listElementInput < b.listElementInput) {
+        return -1;
+      }
+      if (a.listElementInput > b.listElementInput) {
+        return 1;
+      }
       return 0;
-  })
-  setNotes([...tempNotes]);
-  } 
+    });
+    setNotes([...tempNotes]);
+  }
 
   function returnList() {
     const listToReturn = notes.map((value, index) => {
@@ -65,6 +85,7 @@ function ShoppingList() {
         <li key={index}>
           {" "}
           <ListElement
+            del={deleteListElement}
             number={index}
             index={index + 1}
             changeAmount={changeAmount}
@@ -93,10 +114,12 @@ function ShoppingList() {
         name="listPriceInput"
         onChange={handleInputChange}
         value={tempNotes.listPriceInput}
+        min={1}
       />
       <button onClick={addToList}>Dodaj</button>
       <button onClick={sortList}>Sortuj</button>
       <div>{returnList()}</div>
+      <p>Podsumowanie: {sum}zł</p>
     </div>
   );
 }
